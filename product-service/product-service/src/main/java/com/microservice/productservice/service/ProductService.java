0e2f4fa050703ba6929a26263875c06ae81a3dec
astralaria <pawut.j@gmail.com>
2022-08-07 04:23:34 +0700

    Feat(Product) : Add ProductService create , deletebyId + testing

--- product-service/product-service/src/main/java/com/microservice/productservice/service/ProductService.java
+++ product-service/product-service/src/main/java/com/microservice/productservice/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.microservice.productservice.service;
 
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
 import com.microservice.productservice.model.Product;
 import com.microservice.productservice.repository.ProductRepository;
 import lombok.Setter;
@@ -15,6 +16,20 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> deleteProductById(Long id){
+        Optional<Product> oldProduct = productRepository.findById(id);
+        if(oldProduct.isEmpty())
+            return Optional.empty();
+
+        productRepository.deleteById(id);
+
+        return oldProduct;
+    }
+
     public Optional<Product> findProductById(Long id){
         Optional<Product> product = productRepository.findById(id);
         return product;
@@ -23,6 +38,18 @@ public class ProductService {
     public List<Product> findByProductName(String productName){
         List<Product> products = productRepository.findByProductNameContaining(productName);
         return products;
+    }
+
+    public Optional<Product> updateProductByProductId(Product product, Long id){
+        Optional<Product> oldProduct = productRepository.findById(id);
+        if(oldProduct.isEmpty())
+            return Optional.empty();
+        Product newProduct = oldProduct.get();
+        newProduct.setProductName(product.getProductName());
+        newProduct.setPrice(product.getPrice());
+
+        Product result = productRepository.save(newProduct);
+        return Optional.of(result);
     }
 
 }
--- product-service/product-service/src/test/java/com/microservice/productservice/service/ProductServiceTest.java
+++ product-service/product-service/src/test/java/com/microservice/productservice/service/ProductServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -22,6 +23,41 @@ class ProductServiceTest {
 
     @InjectMocks
     private ProductService productService;
+
+
+    @Test
+    void createProduct(){
+        Product product = new Product();
+        product.setProductName("test");
+
+        when(productRepository.save(product)).thenReturn(product);
+
+        Product result = productService.createProduct(product);
+        assertEquals(result.getProductName(),"test");
+    }
+
+    @Test
+    void deleteProductByIdIfNull(){
+        Product product = new Product();
+        when(productRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+        Long id = 0L;
+        Optional<Product> result = productService.deleteProductById(id);
+
+        assertEquals(result,Optional.empty());
+    }
+
+    @Test
+    void deleteProductById(){
+        Product product = new Product();
+        product.setProductName("test");
+        when(productRepository.findById(Mockito.any())).thenReturn(Optional.of(product));
+        Long id = 0L;
+        Optional<Product> result = productService.deleteProductById(id);
+
+        assertEquals(result.get().getProductName(),"test");
+    }
+
+
 
     @Test
     void findProductById() {
@@ -45,4 +81,30 @@ class ProductServiceTest {
 
         assertEquals(result.size() , 1);
     }
+
+    @Test
+    void updateProductByProductIdIfNull(){
+        Product product = new Product();
+        when(productRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+        Long id = 0L;
+        Optional<Product> result = productService.updateProductByProductId(product,id);
+
+        assertEquals(result,Optional.empty());
+    }
+
+    @Test
+    void updateProductByProductId(){
+
+        Product product = new Product();
+        product.setProductName("test");
+        Long id = 0L;
+        when(productRepository.findById(id)).thenReturn(Optional.of(product));
+        when(productRepository.save(product)).thenReturn(product);
+        Optional<Product> result = productService.updateProductByProductId(product,id);
+
+        assertEquals(result.get().getProductName(),"test");
+    }
+
+
+
 }
